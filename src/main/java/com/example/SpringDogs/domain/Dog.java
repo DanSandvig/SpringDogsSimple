6a965b8d7f277703b07a1bf56fcab5ee4eca45bc
Jordan <jordanbenbelaid@hotmail.com>
2021-12-14 13:01:57 +0000

    Domain class with database connection

---
+++ src/main/java/com/example/SpringDogs/domain/Dog.java
@@ -0,0 +1,119 @@
+package com.example.SpringDogs.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+//	@Column(nullable = false)
+	@NotNull(message = "Give the dog a name")
+	private String name;
+	
+	@Column
+	private String breed;
+	
+	@Column
+	private long age;
+	
+	//Default constructor
+	public Dog() {
+		
+	}
+
+	//For testing
+	public Dog(long id, String name, String breed, long age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.breed = breed;
+		this.age = age;
+	}
+	
+	//For creating
+	public Dog(String name, String breed, long age) {
+		super();
+		this.name = name;
+		this.breed = breed;
+		this.age = age;
+	}
+
+	//Getters and Setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public long getAge() {
+		return age;
+	}
+
+	public void setAge(long age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (age ^ (age >>> 32));
+		result = prime * result + ((breed == null) ? 0 : breed.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dog other = (Dog) obj;
+		if (age != other.age)
+			return false;
+		if (breed == null) {
+			if (other.breed != null)
+				return false;
+		} else if (!breed.equals(other.breed))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+}
